30032ad88d57965fd8ec2787067cbbbfd7d59e48
Daniel Carmo <DanyBoss@users.noreply.github.com>
2017-12-18 13:31:34 +0000

    First commit.

---
+++ random.js
@@ -0,0 +1,19 @@
+
+//default max values for the randomizer function,
+//if you have more then 4 images in the folder change these numbers
+var maxNumber = 4;
+var minNumber = 1;
+
+randomizeBackground();
+
+function randomNumber(max=maxNumber, min=minNumber){
+    return Math.floor((Math.random() * max) + min);
+}
+
+function randomizeBackground(){
+    $("html").css("background", "url(res/bgs/bg" + randomNumber() + ".jpg) no-repeat center center fixed");
+}
+
+function randomizeImage(){
+    $("#image").attr("src", "res/images/img" + randomNumber() + ".png");
+}
